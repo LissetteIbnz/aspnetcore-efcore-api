2f841538af42728fda42d469013ff1e7339da8bf
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-08 00:35:02 +0100

    Agregar las configuraciones de las entidades

---
+++ SchoolManager.Persistence/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolManager.Core.Domain;
+
+namespace SchoolManager.Persistence.EntityConfigurations
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            // Mapping for table
+            builder.ToTable("Course");
+            // Set key for entity
+            builder.HasKey(e => e.ID);
+            // Set identity for entity (auto increment)
+            builder.Property(e => e.ID).UseSqlServerIdentityColumn();
+            // Set mapping for columns
+            builder.Property(e => e.ID).HasColumnType("int").IsRequired();
+            builder.Property(e => e.Title).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(e => e.Credits).HasColumnType("smallint").IsRequired();
+            // Add configuration for foreign keys
+            builder
+                .HasOne(p => p.DepartmentFk)
+                .WithMany(b => b.Courses)
+                .HasForeignKey(p => p.DepartmentID);
+        }
+    }
+}
---
+++ SchoolManager.Persistence/EntityConfigurations/StudentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolManager.Core.Domain;
+
+namespace SchoolManager.Persistence.EntityConfigurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            // Mapping for table
+            builder.ToTable("Student");
+            // Set key for entity
+            builder.HasKey(s => s.ID);
+            // Set identity for entity (auto increment)
+            builder.Property(s => s.ID).UseSqlServerIdentityColumn();
+            // Set mapping for columns
+            builder.Property(s => s.ID).HasColumnType("int").IsRequired();
+            builder.Property(s => s.LastName).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(s => s.FirstName).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(s => s.EnrollmentDate).HasColumnType("datetime");
+        }
+    }
+}
