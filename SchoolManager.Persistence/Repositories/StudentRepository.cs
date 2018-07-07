a6d3297037a1d010709a265c8f3b68735c3b0a6c
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-08 00:35:23 +0100

    Agregar repositorios de la capa persistencia

---
+++ SchoolManager.Persistence/Repositories/StudentRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManager.Core.Domain;
+using SchoolManager.Core.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolManager.Persistence.Repositories
+{
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        public StudentRepository(SchoolContext context) : base(context)
+        {
+        }
+
+        public Student GetStudentWithCourses(int id)
+        {
+            return SchoolContext.Students.Include(s => s.Enrollments).SingleOrDefault(s => s.ID == id);
+        }
+
+        public IEnumerable<Student> GetStudentsWithCourse(int pageIndex, int pageSize)
+        {
+            return SchoolContext.Students
+                .Include(s => s.Enrollments)
+                .OrderBy(s => s.LastName)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+
+        public IEnumerable<Student> GetTopStudents(int count)
+        {
+            return SchoolContext.Students
+                .Include(s => s.Enrollments)
+                .Where(s => s.Enrollments.Any())
+                .OrderByDescending(s => s.Enrollments.OrderByDescending(e => e.Grade))
+                .Take(count)
+                .ToList();
+        }
+
+        public SchoolContext SchoolContext
+        {
+            get { return Context as SchoolContext; }
+        }
+    }
+}
