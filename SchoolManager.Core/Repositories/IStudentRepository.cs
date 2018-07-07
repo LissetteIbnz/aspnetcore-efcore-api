3e8f1d3fcfa16b3522fab9229929573d4ef94732
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-07 23:58:33 +0100

    Agregar interface de repositorios

---
+++ SchoolManager.Core/Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManager.Core.Domain;
+using System.Collections.Generic;
+
+namespace SchoolManager.Core.Repositories
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+        Student GetStudentWithCourses(int id);
+        IEnumerable<Student> GetTopStudents(int count);
+        IEnumerable<Student> GetStudentsWithCourse(int pageIndex = 1, int pageSize = 10);
+    }
+}
